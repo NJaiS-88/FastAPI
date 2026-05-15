3b090fb3ae38840da191ddf3fad6a027aa278f99
Jai Sumanth Nekkanti <jaisumanthnekkanti@gmail.com>
2026-05-15 19:02:21 +0530

    Initial FastAPI project setup

---
+++ main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+import json
+app = FastAPI()
+
+def load_data():
+    with open('patients.json', 'r') as f:
+        data = json.load(f)
+    return data
+
+@app.get('/')
+def hello():
+    return {"message": "Welcome to the Patient Health Records API!"}
+
+@app.get('/about')
+def about():
+    return {'message': 'An API to manage patient health records.'}
+
+@app.get('/view')
+def view():
+    data = load_data()
+    return data
