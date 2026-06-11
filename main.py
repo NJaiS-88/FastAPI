d437dec660cfd9b7a648bce556121f7e62b1cfb7
Jai Sumanth Nekkanti <jaisumanthnekkanti@gmail.com>
2026-06-11 13:16:38 +0530

    added create endpoint

--- main.py
+++ main.py
@@ -1,11 +1,44 @@
 from fastapi import FastAPI, Path, HTTPException, Query
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel, Field, computed_field
+from typing import Annotated, Literal
 import json
 app = FastAPI()
+
+class Patient(BaseModel):
+    id: Annotated[str, Field(..., description='id of the patient', examples=['P001'])]
+    name: Annotated[str, Field(..., description="name of the patient")]
+    city: Annotated[str, Field(..., decsription='city of the patient')]
+    age: Annotated[int, Field(..., description='age of the patient', gt=0, lt=120)]
+    gender: Annotated[Literal['male', 'female', 'other'], Field(..., description='gender of the patient')]
+    height: Annotated[float, Field(..., gt=0, decsription='height of the patient')]
+    weight: Annotated[float, Field(..., gt=0, description='weight of the patient')]
+
+    @computed_field
+    @property
+    def bmi(self) -> float:
+        bmi = round(self.weight/(self.height**2), 2)
+        return bmi
+    
+    @computed_field
+    @property
+    def verdict(self) -> str:
+        if self.bmi <18.5:
+            return "underweight"
+        elif self.bmi <30:
+            return "normal"
+        else:
+            return "obese"
 
 def load_data():
     with open('patients.json', 'r') as f:
         data = json.load(f)
     return data
+
+def save_data(data):
+    with open("patients.json", 'w') as f:
+        json.dump(data, f)
+
 
 @app.get('/')
 def hello():
@@ -19,6 +52,17 @@ def about():
 def view():
     data = load_data()
     return data
+
+@app.post('/create')
+def create_patient(patient: Patient):
+    data = load_data()
+    if patient.id in data:
+        raise HTTPException(status_code=400, detail="patient already exists")
+    else:
+        data[patient.id] = patient.model_dump(exclude=["id"]) #converts pydantic object to dictionary
+        save_data(data)
+        return JSONResponse(status_code=201, content={"message": "patient added successfully"})
+
 
 @app.get('/patient/{patient_id}')
 def view_patient(patient_id: str = Path(
